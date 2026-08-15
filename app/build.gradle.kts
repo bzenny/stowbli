7736e378f0be8f317d99f3cf352879c272cb09f9
bzenny <122856656+bzenny@users.noreply.github.com>
2026-08-15 02:37:53 +0000

    Align Android JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,18 @@ android {
         applicationId = "com.stowbli.app"
         minSdk = 26
         targetSdk = 35
+
         versionCode = 1
         versionName = "0.0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
